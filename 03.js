87cfef20585f98dfc6f9d8d67afb63911d44b2cb
osvaldo <osvaldonovelli3@gmail.com>
2023-01-24 13:24:12 -0500

    commit

--- 03.js
+++ 03.js
@@ -14,6 +14,14 @@ function buscarAmigo(amigos, nombre) {
   //  buscarAmigo(amigos, 'toni') debe devolver { nombre: 'toni', edad: 33 };
 
   // Tu código aca:
+  var obj = [
+    { name: 'Max', age: 23 },
+    { name: 'John', age: 20 },
+    { name: 'Caley', age: 18 }
+];
+ 
+var found = obj.find(e => e.name === 'John');
+console.log(found);
   
 }
 
